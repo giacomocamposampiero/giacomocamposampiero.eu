f821f484524e59a9faf3bfff621ab8f50041f8be
giacomocamposampiero <giacomo.camposampiero@hotmail.com>
2022-02-18 11:06:36 +0100

    Further steps

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,17 @@
 module.exports = {
     siteMetadata: {
-      title: `giacomocamposampiero`,
-        siteUrl: `https://www.yourdomain.tld`,
+      title: `giacomocamposampiero`
     },
     plugins: [
-
+      "gatsby-plugin-image",
+      "gatsby-plugin-sharp",
+      `gatsby-transformer-sharp`,
+      {
+        resolve: "gatsby-source-filesystem",
+        options: {
+          name: `blog`,
+          path: `${__dirname}/blog`,
+        }
+      },
     ]
 }
